6c83aa5ac8a89f6ed67094322e1c9b3073ed6e06
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-18 13:48:58 -0600

    Remove loanDao.write to keep it in separate branch

--- oDao/loanDao.js
+++ oDao/loanDao.js
@@ -2,22 +2,6 @@
 
 const db = require('./db.js');
 
-const write = (query, parameters) => {
-    return new Promise((resolve, reject) => {
-        db.beginTransaction(transactionError => {
-            if (transactionError) return reject(transactionError);
-            db.query(query, parameters, (queryError, result) => {
-                if (queryError) {
-                    db.rollback();
-                    return reject(queryError);
-                }
-                db.commit();
-                return resolve(result);
-            })
-        })
-    })
-}
-
 exports.update = loan => {
     const query = 'UPDATE tbl_loan SET dueDate = ?, dateIn = ? ' +
         'WHERE bookId = ? AND cardNo = ? AND branchId = ? AND dateOut = ?',
