d65c8983f797eabffe393d19f6c891f57f21f3c5
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-17 19:08:57 -0600

    Write loanDao.read

--- oDao/loanDao.js
+++ oDao/loanDao.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const db = require('./db.js');
+
+exports.read = (bookId = '%', cardNo = '%', branchId = '%', dateOut = '%') => {
+    query = 'SELECT * FROM tbl_book_loans WHERE bookId LIKE ? \
+    AND cardNo LIKE ? AND branchId LIKE ? AND dateOut LIKE ?;';
+    parameters = [bookId, cardNo, branchId, dateOut];
+    return new Promise((resolve, reject) => {
+        db.query(query, parameters, (error, result) => {
+            return error ? reject(error) : resolve(result);
+        })
+    })
+}
