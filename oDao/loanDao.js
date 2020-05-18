5169df6b58e3137b65c73052bdcaf0ffaf26456b
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-18 11:17:03 -0600

    Write loanDao.write

--- oDao/loanDao.js
+++ oDao/loanDao.js
@@ -1,3 +1,19 @@
 'use strict';
 
 const db = require('./db.js');
+
+const write = (query, parameters) => {
+    return new Promise((resolve, reject) => {
+        db.beginTransaction(transactionError => {
+            if (transactionError) return reject(transactionError);
+            db.query(query, parameters, (queryError, result) => {
+                if (queryError) {
+                    db.rollback();
+                    return reject(queryError);
+                }
+                db.commit();
+                return resolve(result);
+            })
+        })
+    })
+}
