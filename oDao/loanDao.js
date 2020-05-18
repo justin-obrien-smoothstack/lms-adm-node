755aa3bad55c4d4c2c02af13033e6b37963053dc
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-18 13:53:58 -0600

    Correct table name in loanDao.update

--- oDao/loanDao.js
+++ oDao/loanDao.js
@@ -3,7 +3,7 @@
 const db = require('./db.js');
 
 exports.update = loan => {
-    const query = 'UPDATE tbl_loan SET dueDate = ?, dateIn = ? ' +
+    const query = 'UPDATE tbl_book_loans SET dueDate = ?, dateIn = ? ' +
         'WHERE bookId = ? AND cardNo = ? AND branchId = ? AND dateOut = ?',
         parameters = [loan.dueDate, loan.dateIn, loan.bookId, loan.cardNo,
         loan.branchId, loan.dateOut];
