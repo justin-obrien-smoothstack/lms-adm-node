fee8fa4bc888002c6663b3f648b2b7732c1cf352
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-17 19:46:24 -0600

    Add const keyword in loanDao.read

--- oDao/loanDao.js
+++ oDao/loanDao.js
@@ -3,9 +3,9 @@
 const db = require('./db.js');
 
 exports.read = (bookId = '%', cardNo = '%', branchId = '%', dateOut = '%') => {
-    query = 'SELECT * FROM tbl_book_loans WHERE bookId LIKE ? \
-    AND cardNo LIKE ? AND branchId LIKE ? AND dateOut LIKE ?;';
-    parameters = [bookId, cardNo, branchId, dateOut];
+    const query = 'SELECT * FROM tbl_book_loans WHERE bookId LIKE ? ' +
+        'AND cardNo LIKE ? AND branchId LIKE ? AND dateOut LIKE ?;',
+        parameters = [bookId, cardNo, branchId, dateOut];
     return new Promise((resolve, reject) => {
         db.query(query, parameters, (error, result) => {
             return error ? reject(error) : resolve(result);
