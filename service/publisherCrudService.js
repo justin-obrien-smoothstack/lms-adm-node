222f2f1971b57bcdbdd3066846bcf9dfd9d541fc
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 10:45:21 -0600

    Fix bug in publisherCrudService
    Make readAll return a promise

--- service/publisherCrudService.js
+++ service/publisherCrudService.js
@@ -12,16 +12,19 @@ exports.readOne = () => {
 
 exports.readAll = () => {
     const responseAttributes = {};
-    dao.read().then(result => {
-        responseAttributes.status = 200
-        responseAttributes.message = result.length === 0 ?
-            'There are no publishers in the database.' : result;
-    }).catch(error => {
-        responseAttributes.status = 500;
-        responseAttributes.message = 'There was an error while attempting to ' +
-        'read publishers from the database.';
+    return new Promise((resolve, reject) => {
+        dao.read().then(result => {
+            responseAttributes.status = 200
+            responseAttributes.message = result.length === 0 ?
+                'There are no publishers in the database.' : result;
+            resolve(responseAttributes);
+        }).catch(error => {
+            responseAttributes.status = 500;
+            responseAttributes.message = 'There was an error while attempting to ' +
+                'read publishers from the database.';
+            reject(responseAttributes);
+        })
     })
-    return responseAttributes;
 };
 
 exports.update = () => {
