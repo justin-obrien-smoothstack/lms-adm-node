a861346ae41aba31a8ea9dfb63b22cbb2a74c620
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 09:48:32 -0600

    Create skeleton of publisher CRUD features

---
+++ oDao/publisherDao.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const db = require('./db.js');
+
+const write = () => {
+
+};
+
+exports.create = () => {
+
+};
+
+exports.read = () => {
+
+};
+
+exports.update = () => {
+
+};
+
+exports.delete = () => {
+
+};
---
+++ service/publisherCrudService.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const dao = require('../oDao/publisherDao.js');
+
+exports.create = () => {
+
+};
+
+exports.readOne = () => {
+
+};
+
+exports.readAll = () => {
+
+};
+
+exports.update = () => {
+
+};
+
+exports.delete = () => {
+
+};
