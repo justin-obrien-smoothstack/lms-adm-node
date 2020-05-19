ed7c6df57ef09248513333c84b1d4ed91e2e6de0
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-18 21:38:30 -0600

    Write overrideController

--- controller/overrideController.js
+++ controller/overrideController.js
@@ -3,4 +3,15 @@
 const router = require('express').Router(),
     service = reqiure('../service/overrideService');
 
-
+router.put('/lms/admin/loan/book/:bookId/borrower/:cardNo/branch/:branchId' +
+    '/dateout/:dateOut', async (request, response) => {
+        const loanId = {
+            bookId: bookId,
+            cardNo: cardNo,
+            branchId: branchId,
+            dateOut: dateOut
+        }, result = await service.override(loanId);
+        response.status(result.status);
+        response.send(result.message);
+    }
+)
