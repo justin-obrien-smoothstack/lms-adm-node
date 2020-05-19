c042ba63bbe98b0cc54ee78d2ea33fe97053251d
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-18 21:53:19 -0600

    Write main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser'), express = require('express'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.listen(3000);
