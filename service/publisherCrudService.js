4afc99edd9cf6698d7288b6f5eda6b0905ace64f
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 14:07:21 -0600

    Start writing publisherCrudService.create

--- service/publisherCrudService.js
+++ service/publisherCrudService.js
@@ -2,8 +2,19 @@
 
 const dao = require('../oDao/publisherDao.js');
 
-exports.create = () => {
+exports.create = (publisher) => {
+    const responseAttributes = {};
+    return new Promise((resolve, reject) => {
+        if (publisher.publisherName.length > 45 || )
+        dao.create(publisher).then(result => {
+            responseAttributes.status = 201;
+            responseAttributes.message = publisher;
+        }).catch(error => {
 
+        })
+
+        resolve(responseAttributes);
+    })
 };
 
 exports.readOne = () => {
