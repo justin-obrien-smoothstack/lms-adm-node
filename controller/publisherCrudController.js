abb07c26a778c583762e652984dc60513c5a4bb7
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 13:35:31 -0600

    Write function to delete in publisherCrudController

--- controller/publisherCrudController.js
+++ controller/publisherCrudController.js
@@ -19,8 +19,14 @@ router.put('', () => {
 
 });
 
-router.delete('', () => {
-
+router.delete('/lms/admin/publishers/:publisherId', (request, response) => {
+    service.delete(request.params.publisherId).then(result => {
+        response.status(result.status);
+        response.send(result.message);
+    }).catch(error => {
+        response.status(500);
+        response.send('An unknown error occurred.');
+    })
 });
 
 module.exports = router;
