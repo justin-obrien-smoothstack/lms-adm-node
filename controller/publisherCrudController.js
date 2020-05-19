8235ae123fa95ce855161357e01aa7d8ac333632
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 14:30:32 -0600

    Write function to create in publisherCrudController

--- controller/publisherCrudController.js
+++ controller/publisherCrudController.js
@@ -3,8 +3,15 @@
 const router = require('express').Router(),
     service = require('../service/publisherCrudService');
 
-router.post('', () => {
-
+router.post('lms/admin/publishers', (request, response) => {
+    const publisher = request.body;
+    service.create(publisher).then(result => {
+        response.status(result.status);
+        response.send(result.message);
+    }).catch(error => {
+        response.status(500);
+        response.send('An unknown error occurred.');
+    })
 });
 
 router.get('', () => {
