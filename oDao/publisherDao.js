8eaaeb5aa9045797a9ed63970f9235e5189c3980
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 13:02:41 -0600

    Write publisherDao.write

--- oDao/publisherDao.js
+++ oDao/publisherDao.js
@@ -2,8 +2,20 @@
 
 const db = require('./db.js');
 
-const write = () => {
-
+const write = (query, parameters) => {
+    return new Promise((resolve, reject) => {
+        db.beginTransaction(transactionError => {
+            if (transactionError) return reject(transactionError);
+            db.query(query, parameters, (queryError, result) => {
+                if (queryError) {
+                    db.rollback();
+                    return reject(queryError);
+                }
+                db.commit();
+                return resolve(result);
+            })
+        })
+    })
 };
 
 exports.create = () => {
