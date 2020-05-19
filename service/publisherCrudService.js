764569de040dd4fcf636f86f40c918d9139503bd
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 12:42:47 -0600

    Write publisherCrudService.readOne

--- service/publisherCrudService.js
+++ service/publisherCrudService.js
@@ -6,8 +6,21 @@ exports.create = () => {
 
 };
 
-exports.readOne = () => {
-
+exports.readOne = (publisherId) => {
+    const responseAttributes = {};
+    return new Promise((resolve, reject) => {
+        dao.read(publisherId).then(result => {
+            responseAttributes.status = 200
+            responseAttributes.message = result.length === 0 ?
+                'There are no publishers in the database.' : result;
+            resolve(responseAttributes);
+        }).catch(error => {
+            responseAttributes.status = 500;
+            responseAttributes.message = 'There was an error while attempting to ' +
+                'read publishers from the database.';
+            reject(responseAttributes);
+        })
+    })
 };
 
 exports.readAll = () => {
