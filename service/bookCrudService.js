f4960c5bb634718d3532663daff6126c10340b1a
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 20:07:42 -0600

    Write bookCrudService.delete

--- service/bookCrudService.js
+++ service/bookCrudService.js
@@ -20,34 +20,31 @@ exports.update = () => {
 
 exports.delete = (bookId, cb) => {
     const responseAttributes = {};
-    return new Promise((resolve, reject) => {
-        dao.read(bookId).then(result => {
-            if (result.length === 0) {
-                responseAttributes.status = 404;
-                responseAttributes.message =
-                    `There exists no book with ID ${bookId}.`;
-                resolve(responseAttributes);
-            } else {
-                dao.delete(bookId).then(result => {
-                    responseAttributes.status = 200;
-                    responseAttributes.message =
-                        `Book #${bookId} ` +
-                        `was deleted from the database.`;
-                    resolve(responseAttributes);
-                }).catch(error => {
-                    responseAttributes.status = 500;
-                    responseAttributes.message =
-                        'There was an error while attempting to ' +
-                        'delete that book from the database.';
-                    resolve(responseAttributes);
-                })
-            }
-        }).catch(error => {
+    dao.read((error, result) => {
+        if (error) {
             responseAttributes.status = 500;
             responseAttributes.message =
                 'There was an error while attempting to ' +
                 'find that book in the database.';
-            resolve(responseAttributes);
-        })
-    })
+        } else if (result.length === 0) {
+            responseAttributes.status = 404;
+            responseAttributes.message =
+                `There exists no book with ID ${bookId}.`;
+        } else {
+            dao.delete(bookId, (error, result) => {
+                if (error) {
+                    responseAttributes.status = 500;
+                    responseAttributes.message =
+                        'There was an error while attempting to ' +
+                        'delete that book from the database.';
+                } else {
+                    responseAttributes.status = 200;
+                    responseAttributes.message =
+                        `Book #${bookId} ` +
+                        `was deleted from the database.`;
+                }
+            })
+        }
+    }, bookId);
+    cb(responseAttributes);
 };
