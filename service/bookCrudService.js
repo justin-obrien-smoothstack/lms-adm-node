704ffc3be30734c4e5b5ce81245b3f1057ccf1c6
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 19:47:54 -0600

    Start writing bookCrudService.delete

--- service/bookCrudService.js
+++ service/bookCrudService.js
@@ -18,6 +18,36 @@ exports.update = () => {
 
 };
 
-exports.delete = () => {
-
+exports.delete = (bookId, cb) => {
+    const responseAttributes = {};
+    return new Promise((resolve, reject) => {
+        dao.read(bookId).then(result => {
+            if (result.length === 0) {
+                responseAttributes.status = 404;
+                responseAttributes.message =
+                    `There exists no book with ID ${bookId}.`;
+                resolve(responseAttributes);
+            } else {
+                dao.delete(bookId).then(result => {
+                    responseAttributes.status = 200;
+                    responseAttributes.message =
+                        `Book #${bookId} ` +
+                        `was deleted from the database.`;
+                    resolve(responseAttributes);
+                }).catch(error => {
+                    responseAttributes.status = 500;
+                    responseAttributes.message =
+                        'There was an error while attempting to ' +
+                        'delete that book from the database.';
+                    resolve(responseAttributes);
+                })
+            }
+        }).catch(error => {
+            responseAttributes.status = 500;
+            responseAttributes.message =
+                'There was an error while attempting to ' +
+                'find that book in the database.';
+            resolve(responseAttributes);
+        })
+    })
 };
