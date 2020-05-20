d37d8a73a5b03696a386667a9b38e7b7c6969f97
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 20:11:42 -0600

    Clean up bookDao.write

--- oDao/bookDao.js
+++ oDao/bookDao.js
@@ -5,17 +5,11 @@ const db = require('./db.js');
 const write = (query, parameters, cb) => {
     db.beginTransaction(transactionError => {
         if (transactionError) cb(transactionError);
-        else {
-            db.query(query, parameters, (queryError, result) => {
-                if (queryError) {
-                    db.rollback();
-                    cb(queryError);
-                } else {
-                    db.commit();
-                    cb(result);
-                }
-            })
-        }
+        else db.query(query, parameters, (queryError, result) => {
+            if (queryError) db.rollback();
+            else db.commit();
+            cb(queryError, result);
+        })
     })
 };
 
