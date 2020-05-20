caf9dd4c8e1c05fb91b6a146af4bfdfc71b63734
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-19 18:25:30 -0600

    Write bookCrudService.readAll
    But it seems incomplete since it doesn't use the callback passed to it

--- service/bookCrudService.js
+++ service/bookCrudService.js
@@ -10,8 +10,33 @@ exports.readOne = () => {
 
 };
 
-exports.readAll = () => {
+exports.readAll = (cb) => {
+    const responseAttributes = {};
+    dao.read((error, result) => {
+        if (error) {
+            responseAttributes.status = 500;
+            responseAttributes.message = 'There was an error while attempting to ' +
+                'read books from the database.';
+            return responseAttributes;
+        }
+        responseAttributes.status = 200
+        responseAttributes.message = result.length === 0 ?
+            'There are no publishers in the database.' : result;
+        return responseAttributes;
+    })
+}
 
+.then(result => {
+    responseAttributes.status = 200
+    responseAttributes.message = result.length === 0 ?
+        'There are no publishers in the database.' : result;
+    resolve(responseAttributes);
+}).catch(error => {
+    responseAttributes.status = 500;
+    responseAttributes.message = 'There was an error while attempting to ' +
+        'read publishers from the database.';
+    resolve(responseAttributes);
+})
 };
 
 exports.update = () => {
