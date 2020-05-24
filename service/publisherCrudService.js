127fe8fe7db1c8e41f02aa5c655344c9d886400c
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 11:02:19 -0600

    Write publisherCrudService.deletePublisher

--- service/publisherCrudService.js
+++ service/publisherCrudService.js
@@ -13,3 +13,33 @@ exports.readPublishers = () => {
     );
   });
 };
+
+exports.deletePublisher = (publisherId) => {
+  const results = {
+    readError: false,
+    publisherNotFound: false,
+    deleteError: false,
+  };
+  return new Promise((resolve, reject) => {
+    publisherDao.readPublishers(db, publisherId).then(
+      (readResult) => {
+        if (readResult.length === 0) {
+          results.publisherNotFound = true;
+          reject(results);
+          return;
+        }
+        publisherDao.deletePublisher(publisherId).then(
+          (deleteResult) => resolve(results),
+          (deleteError) => {
+            results.deleteError = true;
+            reject(results);
+          }
+        );
+      },
+      (readError) => {
+        results.readError = true;
+        reject(results);
+      }
+    );
+  });
+};
