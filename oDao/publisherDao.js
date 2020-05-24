2287f4f1e82db068c675018269d9ffecc51fc587
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 12:13:01 -0600

    Write publisherDao.updatePublisher

--- oDao/publisherDao.js
+++ oDao/publisherDao.js
@@ -1,1 +1,16 @@
 "use strict";
+
+exports.updatePublisher = (db, publisher) => {
+  const query =
+      "UPDATE tbl_publisher SET publisherName = ?, publisherAddress = ?, publisherPhone = ? WHERE publisherId = ?;",
+    parameters = [
+      publisher.publisherName,
+      publisher.publisherAddress,
+      publisher.publisherPhone,
+      publisher.publisherId,
+    ];
+  db.query(query, parameters, (error, result) => {
+    if (error) reject(error);
+    resolve(result);
+  });
+};
