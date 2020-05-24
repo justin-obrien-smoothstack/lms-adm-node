37a16af20359be8c7765898d9b2e14fe3efa8022
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 16:46:31 -0600

    Write bookDao.createBook

--- oDao/bookDao.js
+++ oDao/bookDao.js
@@ -1,1 +1,12 @@
 "use strict";
+
+exports.createBook = (db, book) => {
+  const query = "INSERT INTO tbl_book (title, pubId) VALUES (?,?);",
+    parameters = [book.title, book.pubId];
+  return new Promise((resolve, reject) => {
+    db.query(query, parameters, (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
