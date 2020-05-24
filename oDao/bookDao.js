1af9d814d90cdf8cdee00fb7ad099a077fc3c44c
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 14:17:31 -0600

    Write bookDao.deleteBook

--- oDao/bookDao.js
+++ oDao/bookDao.js
@@ -1,1 +1,11 @@
 "use strict";
+
+exports.deleteBook = (db, bookId) => {
+  const query = "DELETE FROM tbl_book WHERE bookId = ?;";
+  return new Promise((resolve, reject) => {
+    db.query(query, [bookId], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
