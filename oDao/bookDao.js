e10f9d7f0e011c72f643ad0ff299b56d3a781319
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 17:36:07 -0600

    Write bookDao.updateBook

--- oDao/bookDao.js
+++ oDao/bookDao.js
@@ -1,1 +1,12 @@
 "use strict";
+
+exports.updateBook = (db, book) => {
+  const query = "UPDATE tbl_book SET title = ?, pubId = ? WHERE bookId = ?;",
+    parameters = [book.title, book.pubId, book.bookId];
+  return new Promise((resolve, reject) => {
+    db.query(query, parameters, (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
