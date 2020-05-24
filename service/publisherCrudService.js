ed0fe968619a5f85b627e8266413666de276df9f
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 11:50:48 -0600

    Write publisherCrudService.createPublisher

--- service/publisherCrudService.js
+++ service/publisherCrudService.js
@@ -5,11 +5,42 @@ const db = require("./db"),
 
 const maxLength = 45;
 
-exports.createPublisher = (publisher)=>{
-    const results = {};
-    return new Promise((resolve, reject)=>{
-        if(!publisher.publisherName){
-            results.noName = true;
+exports.createPublisher = (publisher) => {
+  const results = {
+    noName: false,
+    tooLong: false,
+    transactionError: false,
+    createError: false,
+  };
+  return new Promise((resolve, reject) => {
+    if (!publisher.publisherName) {
+      results.noName = true;
+      reject(results);
+      return;
+    }
+    if (
+      publisher.publisherName.length > maxLength ||
+      (publisher.publisherAddress &&
+        publisher.publisherAddress.length > maxLength) ||
+      (publisher.publisherPhone && publisher.publisherPhone.length > maxLength)
+    ) {
+      results.tooLong = true;
+      reject(results);
+      return;
+    }
+    db.beginTransaction((transactionError) => {
+      if (transactionError) {
+        results.transactionError = true;
+        reject(results);
+        return;
+      }
+      publisherDao.createPublisher(db, publisher).then(
+        (createResult) => resolve(results),
+        (createError) => {
+          results.createError = true;
+          reject(results);
         }
-    })
-}
+      );
+    });
+  });
+};
