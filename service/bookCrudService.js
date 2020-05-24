ca900c440efc2ef64b5039b232b01b96a3c9943d
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 14:39:47 -0600

    Write bookCrudService.deleteBook

--- service/bookCrudService.js
+++ service/bookCrudService.js
@@ -5,3 +5,38 @@ const db = require("./db"),
   publisherDao = require("../oDao/publisherDao");
 
 const maxLength = 45;
+
+exports.delete = async (bookId) => {
+  const results = {
+    transactionError: true,
+    readError: false,
+    bookNotFound: false,
+  };
+  let books;
+  db.beginTransaction((transactionError) => {
+    if (transactionError) {
+      results.transactionError = true;
+      throw results;
+    }
+    try {
+      books = await bookDao.readBooks(db, bookId);
+    } catch (readError) {
+      results.readError = true;
+      db.rollback(() => {
+        throw results;
+      });
+    }
+    if (books.length === 0) {
+      results.bookNotFound = true;
+      db.rollback(() => {
+        throw results;
+      });
+    }
+    try {
+      return await bookDao.deleteBook(db, bookId);
+    } catch (deleteError) {
+        results.deleteError =true;
+        throw results;
+    }
+  });
+};
