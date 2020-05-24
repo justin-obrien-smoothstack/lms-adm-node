bac374fc087eea6edb5100500e0a799c2a223e97
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-23 21:21:59 -0600

    Write publisherCrudService.readPublishers

--- service/publisherCrudService.js
+++ service/publisherCrudService.js
@@ -4,3 +4,13 @@ const db = require("./db"),
   publisherDao = require("../oDao/publisherDao");
 
 const maxLength = 45;
+
+exports.readPublishers = () => {
+  const results = {};
+  return new Promise((resolve, reject) => {
+    publisherDao.readPublishers(db).then(
+      (result) => resolve(result),
+      (error) => reject(error)
+    );
+  });
+};
