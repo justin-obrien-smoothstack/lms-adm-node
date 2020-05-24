825dbbcbd5a55ec2aa41a51a5b0207443d8b1f69
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-23 21:10:54 -0600

    Write publisherDao.read

--- oDao/publisherDao.js
+++ oDao/publisherDao.js
@@ -1,1 +1,11 @@
 "use strict";
+
+exports.read = (db, publisherId = "%") => {
+  const query = "SELECT * FROM tbl_publisher WHERE publisherId LIKE ?";
+  return new Promise((resolve, reject) => {
+    db.query(query, [publisherId], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
