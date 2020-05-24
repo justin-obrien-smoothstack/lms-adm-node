b239b681f7644cc303a2471b91cf7e1cd210f56e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 14:12:14 -0600

    Change scope of try in bookCrudController
    Make it apply only to the await

--- controller/bookCrudController.js
+++ controller/bookCrudController.js
@@ -7,18 +7,19 @@ router.get("/lms/admin/books", async (request, response) => {
   let books;
   try {
     books = await bookCrudService.readBooks();
-    response.status(200);
-    response.format({
-      "application/json": () => response.send(books),
-      "application/xml": () => response.send(jsontoxml(books)),
-    });
   } catch (error) {
     response
       .status(500)
       .send(
         "There was an error while attempting to retrieve book information from the database."
       );
+      return;
   }
+  response.status(200);
+  response.format({
+    "application/json": () => response.send(books),
+    "application/xml": () => response.send(jsontoxml(books)),
+  });
 });
 
 module.exports = router;
