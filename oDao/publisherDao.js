baf52cff9bf3d2bec006c84093b9527b49cc48e4
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 12:46:13 -0600

    Add publisherDao.readPublishers

--- oDao/publisherDao.js
+++ oDao/publisherDao.js
@@ -1,5 +1,15 @@
 "use strict";
 
+exports.readPublishers = (db, publisherId = "%") => {
+  const query = "SELECT * FROM tbl_publisher WHERE publisherId LIKE ?";
+  return new Promise((resolve, reject) => {
+    db.query(query, [publisherId], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
+
 exports.updatePublisher = (db, publisher) => {
   const query =
       "UPDATE tbl_publisher SET publisherName = ?, publisherAddress = ?, publisherPhone = ? WHERE publisherId = ?;",
