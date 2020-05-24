627e1edf59569b7d72794cce90a99e353420a471
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 13:11:27 -0600

    Write bookDao.readBooks

--- oDao/bookDao.js
+++ oDao/bookDao.js
@@ -1,1 +1,11 @@
 "use strict";
+
+exports.readBooks = (db, bookId = "%") => {
+  const query = "SELECT * FROM tbl_book WHERE bookId LIKE ?";
+  return new Promise((resolve, reject) => {
+    db.query(query, [bookId], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
