6fd74238f4dd1fd427fe5362b50198ad5fc18d83
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-23 21:33:32 -0600

    Write read function in publisherCrudController

--- controller/publisherCrudController.js
+++ controller/publisherCrudController.js
@@ -4,4 +4,23 @@ const publisherCrudService = require("../service/publisherCrudService"),
   router = require("express").Router(),
   jsontoxml = require("jsontoxml");
 
+router.get("/lms/admin/publishers", (request, response) => {
+  publisherCrudService.readPublishers().then(
+    (result) => {
+      response.status(200);
+      response.format({
+        "application/json": () => response.send(result),
+        "application/xml": () => response.send(jsontoxml(result)),
+      });
+    },
+    (error) => {
+      response
+        .status(500)
+        .send(
+          "There was an error while attempting to retrieve publisher information from the databse."
+        );
+    }
+  );
+});
+
 module.exports = router;
