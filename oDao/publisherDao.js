e69eb3cc7da15fa7ef6afa8505f033dfc906b838
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-24 11:28:06 -0600

    Write publisherDao.createPublisher

--- oDao/publisherDao.js
+++ oDao/publisherDao.js
@@ -1,1 +1,18 @@
 "use strict";
+
+exports.createPublisher = (db, publisher) => {
+  const query =
+      "INSERT INTO tbl_publisher " +
+      "(publisherName, publisherAddress, publisherPhone) VALUES (?, ?, ?);",
+    parameters = [
+      publisher.publisherName,
+      publisher.publisherAddress,
+      publisher.publisherPhone,
+    ];
+  return new Promise((resolve, reject) => {
+    db.query(query, parameters, (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
