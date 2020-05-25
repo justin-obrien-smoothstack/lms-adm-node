5fb39f0029c62480f5663c98f84734f1a27a0bec
sean-darsie <sean.darsie@smoothstack.com>
2020-05-25 11:23:45 -0700

    moved error handling for author and branch

--- service/authorService.js
+++ service/authorService.js
@@ -1,29 +1,19 @@
 const authorDao = require("../sDao/authorDao");
-
-const maxLength = 45;
-const minLength = 2;
+const db = require("./db");
 
 exports.readAuthor = (id) => {
     return new Promise( (resolve, reject) => {
-        let responseAttributes = {};
-        authorDao.read(id)
+        authorDao.read(db,id)
         .then((result) => {
-            if (result.length == 0) {
-                responseAttributes.status = 404;
-                responseAttributes.message = `author with id: ${id} does not exist`;
-                resolve(responseAttributes);
-            } else {
-                authorDao.readBooksByAuthor(id)
-                .then((res) => {
-                    result[0]['books'] = res;
-                    responseAttributes.status = 200;
-                    responseAttributes.message = result;
-                    resolve(responseAttributes);
-                }) 
-                .catch((err) => {
-                    reject(err);
-                });
-            }
+            authorDao.readBooksByAuthor(db,id)
+            .then((res) => {
+                result[0]['books'] = res;
+                resolve(result);
+            }) 
+            .catch((err) => {
+                reject(err);
+            });
+        
         })
         .catch((error) => {
             console.log(error);
@@ -34,7 +24,7 @@ exports.readAuthor = (id) => {
 
 let addBooksToAuthors = async (result) => {
     return new Promise((resolve, reject) => {
-        authorDao.readBooksByAuthor(result['authorId'])
+        authorDao.readBooksByAuthor(db,result['authorId'])
         .then((res) => {
             result['books'] = res;
             resolve(result);
@@ -47,21 +37,12 @@ let addBooksToAuthors = async (result) => {
 
 exports.readAll = async () => {
     return new Promise( (resolve, reject) => {
-        let responseAttributes = {};
-        authorDao.readAll()
+        authorDao.readAll(db)
         .then(async (result) => {
-            if (result.length == 0) {
-                responseAttributes.status = 404;
-                responseAttributes.message = `no author records exist`;
-                resolve(responseAttributes);
-            } else {
-                for (let i = 0; i < result.length; i++) {
-                    result[i] = await addBooksToAuthors(result[i]);
-                }
-                responseAttributes.status = 200;
-                responseAttributes.message = result;
-                resolve(responseAttributes);
+            for (let i = 0; i < result.length; i++) {
+                result[i] = await addBooksToAuthors(result[i]);
             }
+            resolve(result);
         })
         .catch((error) => {
             console.log(error);
@@ -74,26 +55,19 @@ exports.readAll = async () => {
 exports.createAuthor = (author) => {
     return new Promise((resolve, reject) => {
         let responseAttributes = {};
-        if (author.authorName == undefined) {
-            responseAttributes.status = 400;
-            responseAttributes.message = "author name cannot be undefined";
-            resolve(responseAttributes);
-        }
-
-        if (author.authorName.length >= maxLength ||
-            author.authorName.length < minLength) {
-            responseAttributes.status = 400;
-            responseAttributes.message = `all text fields must be between ${minLength} and ${maxLength}`;
-            resolve(responseAttributes);
-        }
-
-        authorDao.create(author)
-        .then((result) =>{
-            responseAttributes.status = 201;
-            responseAttributes.message = `author with name ${author.authorName} created`;
-            resolve(responseAttributes);                })
-        .catch((error) => {
-            reject(error);
+        db.beginTransaction((transactionError) => {
+            if (transactionError) {
+              results.transactionError = true;
+              reject(results);
+              return;
+            }
+            authorDao.create(db,author)
+            .then((result) =>{
+                responseAttributes.message = `author with name ${author.authorName} created`;
+                resolve(responseAttributes);                })
+            .catch((error) => {
+                reject(error);
+            });
         });
     });
 };
@@ -101,39 +75,34 @@ exports.createAuthor = (author) => {
 exports.updateAuthor = (author) => {
     return new Promise((resolve, reject) => {
         let responseAttributes = {};
-        if (author.authorName == undefined || author.authorId == undefined) {
-            responseAttributes.status = 400;
-            responseAttributes.message = "neither author name or id can be undefined";
-            resolve(responseAttributes);
-        }
 
-        if (author.authorName.length >= maxLength ||
-            author.authorName.length < minLength) {
-            responseAttributes.status = 400;
-            responseAttributes.message = `all text fields must be between ${minLength} and ${maxLength} characters`;
-            resolve(responseAttributes);
-        }
-        
-        authorDao.read(author.authorId)
-        .then((result) => {
-            if (result.length == 0) { // no record was found
-                responseAttributes.status = 404;
-                responseAttributes.message = `no author with id ${author.authorId} was found`;
-                resolve(responseAttributes);
-            } else {
-                authorDao.update(author)
-                .then((result) =>{
-                    responseAttributes.status = 202;
-                    responseAttributes.message = `author with name ${author.authorName} updated`;
-                    resolve(responseAttributes);                
-                })
-                .catch((error) => {
-                    reject(error);
-                });
+        db.beginTransaction((transactionError) => {
+            if (transactionError) {
+              results.transactionError = true;
+              reject(results);
+              return;
             }
-        })
-        .catch((error) => {
-            reject(error);
+            authorDao.read(db, author.authorId)
+            .then((result) => {
+                if (result.length == 0) { // no record was found
+                    responseAttributes.status = 404;
+                    responseAttributes.message = `no author with id ${author.authorId} was found`;
+                    resolve(responseAttributes);
+                } else {
+                    authorDao.update(db, author)
+                    .then((result) =>{
+                        responseAttributes.status = 202;
+                        responseAttributes.message = `author with name ${author.authorName} updated`;
+                        resolve(responseAttributes);                
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
         });
     });
 };
@@ -142,26 +111,28 @@ exports.deleteAuthor = (authorId) => {
     return new Promise((resolve, reject) => {
         let responseAttributes = {};
 
-        if (authorId == undefined) {
-            responseAttributes.status = 400;
-            responseAttributes.message = "author id cannot be undefined";
-            resolve(responseAttributes);
-        }
-        authorDao.read(authorId)
+        authorDao.read(db, authorId)
         .then((result) => {
             if (result.length == 0) { // no record was found
                 responseAttributes.status = 404;
                 responseAttributes.message = `no author with id ${authorId} was found`;
                 resolve(responseAttributes);
             } else {
-                authorDao.delete(authorId)
-                .then((result) =>{
-                    responseAttributes.status = 204;
-                    responseAttributes.message = `author with id: ${authorId} was deleted`;
-                    resolve(responseAttributes);                
-                })
-                .catch((error) => {
-                    reject(error);
+                db.beginTransaction((transactionError) => {
+                    if (transactionError) {
+                      results.transactionError = true;
+                      reject(results);
+                      return;
+                    }
+                    authorDao.delete(db,authorId)
+                    .then((result) =>{
+                        responseAttributes.status = 204;
+                        responseAttributes.message = `author with id: ${authorId} was deleted`;
+                        resolve(responseAttributes);                
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
                 });
             }
         })
--- service/branchService.js
+++ service/branchService.js
@@ -1,22 +1,14 @@
 const branchDao = require("../sDao/branchDao");
+const db = require("./db");
 
 const maxLength = 45;
 const minLength = 2;
 
 exports.readBranch = (id) => {
     return new Promise( (resolve, reject) => {
-        let responseAttributes = {};
-        branchDao.read(id)
+        branchDao.read(db,id)
         .then((result) => {
-            if (result.length == 0) {
-                responseAttributes.status = 404;
-                responseAttributes.message = `branch with id: ${id} does not exist`;
-                resolve(responseAttributes);
-            } else {
-                responseAttributes.status = 200;
-                responseAttributes.message = result;
-                resolve(responseAttributes);
-            }
+            resolve(result);
         })
         .catch((error) => {
             console.log(error);
@@ -28,17 +20,9 @@ exports.readBranch = (id) => {
 exports.readAll = () => {
     return new Promise( (resolve, reject) => {
         let responseAttributes = {};
-        branchDao.readAll()
+        branchDao.readAll(db)
         .then((result) => {
-            if (result.length == 0) {
-                responseAttributes.status = 404;
-                responseAttributes.message = `no branch records exist`;
-                resolve(responseAttributes);
-            } else {
-                responseAttributes.status = 200;
-                responseAttributes.message = result;
-                resolve(responseAttributes);
-            }
+           resolve(result);
         })
         .catch((error) => {
             console.log(error);
@@ -51,28 +35,21 @@ exports.readAll = () => {
 exports.createBranch = (branch) => {
     return new Promise((resolve, reject) => {
         let responseAttributes = {};
-        if (branch.branchName == undefined) {
-            responseAttributes.status = 400;
-            responseAttributes.message = "branch name cannot be undefined";
-            resolve(responseAttributes);
-        }
 
-        if (branch.branchName.length >= maxLength ||
-            branch.branchName.length < minLength ||
-            branch.branchAddress.length >= maxLength ||
-            branch.branchAddress.length < minLength) {
-            responseAttributes.status = 400;
-            responseAttributes.message = `all text fields must be between ${minLength} and ${maxLength}`;
-            resolve(responseAttributes);
-        }
-
-        branchDao.create(branch)
-        .then((result) =>{
-            responseAttributes.status = 201;
-            responseAttributes.message = `branch with name ${branch.branchName} created`;
-            resolve(responseAttributes);                })
-        .catch((error) => {
-            reject(error);
+        db.beginTransaction((transactionError) => {
+            if (transactionError) {
+              results.transactionError = true;
+              reject(results);
+              return;
+            }
+            branchDao.create(db, branch)
+            .then((result) =>{
+                responseAttributes.status = 201;
+                responseAttributes.message = `branch with name ${branch.branchName} created`;
+                resolve(responseAttributes);                })
+            .catch((error) => {
+                reject(error);
+            });
         });
     });
 };
@@ -80,36 +57,29 @@ exports.createBranch = (branch) => {
 exports.updateBranch = (branch) => {
     return new Promise((resolve, reject) => {
         let responseAttributes = {};
-        if (branch.branchName == undefined || branch.branchId == undefined || branch.branchAddress == undefined) {
-            responseAttributes.status = 400;
-            responseAttributes.message = "neither branch name, id, nor address can be undefined";
-            resolve(responseAttributes);
-        }
-
-        if (branch.branchName.length >= maxLength ||
-            branch.branchName.length < minLength  ||
-            branch.branchAddress.length >= maxLength ||
-            branch.branchAddress.length < minLength) { 
-            responseAttributes.status = 400;
-            responseAttributes.message = `all text fields must be between ${minLength} and ${maxLength} characters`;
-            resolve(responseAttributes);
-        }
         
-        branchDao.read(branch.branchId)
+        branchDao.read(db, branch.branchId)
         .then((result) => {
             if (result.length == 0) { // no record was found
                 responseAttributes.status = 404;
                 responseAttributes.message = `no branch with id ${branch.branchId} was found`;
                 resolve(responseAttributes);
             } else {
-                branchDao.update(branch)
-                .then((result) =>{
-                    responseAttributes.status = 202;
-                    responseAttributes.message = `branch with name ${branch.branchName} updated`;
-                    resolve(responseAttributes);                
-                })
-                .catch((error) => {
-                    reject(error);
+                db.beginTransaction((transactionError) => {
+                    if (transactionError) {
+                      results.transactionError = true;
+                      reject(results);
+                      return;
+                    }
+                    branchDao.update(db, branch)
+                    .then((result) =>{
+                        responseAttributes.status = 202;
+                        responseAttributes.message = `branch with name ${branch.branchName} updated`;
+                        resolve(responseAttributes);                
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
                 });
             }
         })
@@ -123,27 +93,28 @@ exports.deleteBranch = (branchId) => {
     return new Promise((resolve, reject) => {
         let responseAttributes = {};
 
-        if (branchId == undefined) {
-            responseAttributes.status = 400;
-            responseAttributes.message = "branch id cannot be undefined";
-            resolve(responseAttributes);
-        }
-
-        branchDao.read(branchId)
+        branchDao.read(db, branchId)
         .then((result) => {
             if (result.length == 0) { // no record was found
                 responseAttributes.status = 404;
                 responseAttributes.message = `no branch with id ${branchId} was found`;
                 resolve(responseAttributes);
             } else {
-                branchDao.delete(branchId)
-                .then((result) =>{
-                    responseAttributes.status = 204;
-                    responseAttributes.message = `branch with id: ${branchId} was deleted`;
-                    resolve(responseAttributes);                
-                })
-                .catch((error) => {
-                    reject(error);
+                db.beginTransaction((transactionError) => {
+                    if (transactionError) {
+                      results.transactionError = true;
+                      reject(results);
+                      return;
+                    }
+                    branchDao.delete(db,branchId)
+                    .then((result) =>{
+                        responseAttributes.status = 204;
+                        responseAttributes.message = `branch with id: ${branchId} was deleted`;
+                        resolve(responseAttributes);                
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
                 });
             }
         })
