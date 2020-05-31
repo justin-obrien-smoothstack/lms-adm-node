ffd647abd23188a59daa2a300dd0f80700b81bc2
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-30 20:27:09 -0600

    Modify create function in publisherCrudController
    Make it use  async/await instead of .then

--- controller/publisherCrudController.js
+++ controller/publisherCrudController.js
@@ -4,36 +4,36 @@ const publisherCrudService = require("../service/publisherCrudService"),
   router = require("express").Router(),
   jsontoxml = require("jsontoxml");
 
-router.post("/lms/admin/publisher", (request, response) => {
-  const publisher = request.body;
-  publisherCrudService.createPublisher(publisher).then(
-    (result) => response.sendStatus(201),
-    (error) => {
-      if (error.noName) {
-        response.status(400).send("The field 'publisherName' is required.");
-        return;
-      }
-      if (error.tooLong) {
-        response.status(400).send("The maximum field length is 45 characters.");
-        return;
-      }
-      if (error.transactionError) {
-        response
-          .status(500)
-          .send(
-            "There was an error while attempting to start a database transaction."
-          );
-        return;
-      }
-      if (error.createError) {
-        response
-          .status(500)
-          .send(
-            "There was an error while attempting to add the publisher information."
-          );
-      }
+router.post("/lms/admin/publisher", async (request, response) => {
+  try {
+    await publisherCrudService.createPublisher(request.body);
+  } catch (error) {
+    if (error.noName) {
+      response.status(400).send("The field 'publisherName' is required.");
+      return;
     }
-  );
+    if (error.tooLong) {
+      response.status(400).send("The maximum field length is 45 characters.");
+      return;
+    }
+    if (error.transactionError) {
+      response
+        .status(500)
+        .send(
+          "There was an error while attempting to start a database transaction."
+        );
+      return;
+    }
+    if (error.createError) {
+      response
+        .status(500)
+        .send(
+          "There was an error while attempting to add the publisher information."
+        );
+      return;
+    }
+  }
+  response.sendStatus(201);
 });
 
 router.get("/lms/admin/publishers", async (request, response) => {
