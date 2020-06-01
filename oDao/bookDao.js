a012e191cba43608688fcf4aaa0fcdd2a9f4cb27
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-01 17:09:36 -0600

    Write & use bookDao.doQuery to reduce verbosity

--- oDao/bookDao.js
+++ oDao/bookDao.js
@@ -1,8 +1,6 @@
 "use strict";
 
-exports.createBook = (db, book) => {
-  const query = "INSERT INTO tbl_book (title, pubId) VALUES (?,?);",
-    parameters = [book.title, book.pubId];
+const doQuery = (db, query, parameters) => {
   return new Promise((resolve, reject) => {
     db.query(query, parameters, (error, result) => {
       if (error) reject(error);
@@ -11,33 +9,24 @@ exports.createBook = (db, book) => {
   });
 };
 
+exports.createBook = (db, book) => {
+  const query = "INSERT INTO tbl_book (title, pubId) VALUES (?,?);",
+    parameters = [book.title, book.pubId];
+  return doQuery(db, query, parameters);
+};
+
 exports.readBooks = (db, bookId = "%") => {
   const query = "SELECT * FROM tbl_book WHERE bookId LIKE ?";
-  return new Promise((resolve, reject) => {
-    db.query(query, [bookId], (error, result) => {
-      if (error) reject(error);
-      resolve(result);
-    });
-  });
+  return doQuery(db, query, bookId);
 };
 
 exports.updateBook = (db, book) => {
   const query = "UPDATE tbl_book SET title = ?, pubId = ? WHERE bookId = ?;",
     parameters = [book.title, book.pubId, book.bookId];
-  return new Promise((resolve, reject) => {
-    db.query(query, parameters, (error, result) => {
-      if (error) reject(error);
-      resolve(result);
-    });
-  });
+  return doQuery(db, query, parameters);
 };
 
 exports.deleteBook = (db, bookId) => {
   const query = "DELETE FROM tbl_book WHERE bookId = ?;";
-  return new Promise((resolve, reject) => {
-    db.query(query, [bookId], (error, result) => {
-      if (error) reject(error);
-      resolve(result);
-    });
-  });
+  return doQuery(db, query, bookId);
 };
