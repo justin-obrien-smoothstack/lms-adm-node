07877f1673b3f4ae14240380f7330ba8b342ec73
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-02 18:09:22 -0600

    Reorder book controller functions

--- controller/bookCrudController.js
+++ controller/bookCrudController.js
@@ -4,65 +4,6 @@ const bookCrudService = require("../service/bookCrudService"),
   router = require("express").Router(),
   jsontoxml = require("jsontoxml");
 
-router.get("/lms/admin/books", async (request, response) => {
-  let books;
-  try {
-    books = await bookCrudService.readBooks();
-  } catch (error) {
-    response
-      .status(500)
-      .send(
-        "There was an error while attempting to retrieve book information from the database."
-      );
-    return;
-  }
-  response.status(200);
-  response.format({
-    "application/json": () => response.send(books),
-    "application/xml": () => response.send(jsontoxml(books)),
-  });
-});
-
-router.delete("/lms/admin/books/:bookId", async (request, response) => {
-  try {
-    await bookCrudService.deleteBook(request.params.bookId);
-  } catch (error) {
-    if (error.transactionError) {
-      response
-        .status(500)
-        .send(
-          "There was an error while attempting to start a database transaction."
-        );
-      return;
-    }
-    if (error.readError) {
-      response
-        .status(500)
-        .send(
-          "There was an error while attempting to find the book in the database."
-        );
-      return;
-    }
-    if (error.bookNotFound) {
-      response
-        .status(404)
-        .send(
-          `There is no book with ID ${request.params.bookId} in the database.`
-        );
-      return;
-    }
-    if (error.deleteError) {
-      response
-        .status(500)
-        .send(
-          "There was an error while attempting to delete the book information from the database."
-        );
-      return;
-    }
-  }
-  response.sendStatus(204);
-});
-
 router.post("/lms/admin/book", async (request, response) => {
   try {
     await bookCrudService.createBook(request.body);
@@ -109,6 +50,25 @@ router.post("/lms/admin/book", async (request, response) => {
     }
   }
   response.sendStatus(201);
+});
+
+router.get("/lms/admin/books", async (request, response) => {
+  let books;
+  try {
+    books = await bookCrudService.readBooks();
+  } catch (error) {
+    response
+      .status(500)
+      .send(
+        "There was an error while attempting to retrieve book information from the database."
+      );
+    return;
+  }
+  response.status(200);
+  response.format({
+    "application/json": () => response.send(books),
+    "application/xml": () => response.send(jsontoxml(books)),
+  });
 });
 
 router.put("/lms/admin/book", async (request, response) => {
@@ -170,6 +130,46 @@ router.put("/lms/admin/book", async (request, response) => {
         .status(500)
         .send(
           "There was an error while attempting to update the book information."
+        );
+      return;
+    }
+  }
+  response.sendStatus(204);
+});
+
+router.delete("/lms/admin/books/:bookId", async (request, response) => {
+  try {
+    await bookCrudService.deleteBook(request.params.bookId);
+  } catch (error) {
+    if (error.transactionError) {
+      response
+        .status(500)
+        .send(
+          "There was an error while attempting to start a database transaction."
+        );
+      return;
+    }
+    if (error.readError) {
+      response
+        .status(500)
+        .send(
+          "There was an error while attempting to find the book in the database."
+        );
+      return;
+    }
+    if (error.bookNotFound) {
+      response
+        .status(404)
+        .send(
+          `There is no book with ID ${request.params.bookId} in the database.`
+        );
+      return;
+    }
+    if (error.deleteError) {
+      response
+        .status(500)
+        .send(
+          "There was an error while attempting to delete the book information from the database."
         );
       return;
     }
